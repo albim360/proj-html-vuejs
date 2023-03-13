f904a218967c12d4265c1aa4aad5280bf1b7873d
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-13 16:48:33 +0100

    FIX nomi components

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <AppHeader />
         <FreeGuide />
-        <FeatureGuide />
+        <FeaturedCourses />
         <AppFooter />
     </div>
 </template>
@@ -10,7 +10,7 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue'
 import FreeGuide from './components/FreeGuide.vue'
-import FeatureGuide from './components/FeatureGuide.vue'
+import FeaturedCourses from './components/FeaturedCourses.vue'
 import AppFooter from './components/AppFooter.vue'
 </script>
 
